74d92173a62a218e9849705a4e2564258fb71b1c
Tim Zababurin <raccoontd@mail.ru>
2022-02-15 13:39:14 +0300

    tags-ver2.0.2

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 const test = {
-    main: ['example', 'example'],
-    high: ['example', 'example'],
-    mid: ['example', 'example'],
-    low: ['example', 'example']
+    main: ['example'],
+    high: ['example'],
+    mid: ['example'],
+    low: ['example']
 }
 const url = 'https://620a9a0692946600171c5b7f.mockapi.io/db/tags/';
 const main = document.querySelector('#main');
@@ -11,14 +11,29 @@ const mid = document.querySelector('#mid');
 const low = document.querySelector('#low');
 const id = 1;
 
+const item = (i) => {
+    return `<li>
+        <div class='item'>
+            <div class='hash'>#</div>
+            <input class='tag' value=${i}>
+            <div class='del' onclick="DELETE()">x</div>
+        </div>
+    </li>`
+}
+
+let inner = ''; test.main.map(i => inner += item(i)); main.innerHTML = inner;
+test.high.map(i => inner += item(i)); high.innerHTML = inner;
+test.mid.map(i => inner += item(i)); mid.innerHTML = inner;
+test.low.map(i => inner += item(i)); low.innerHTML = inner;
+
 fetch(url)
     .then(response => response.json())
     .then(commits => {
         console.log(commits[id])
-        main.innerHTML = commits[id].main.join('<br>');
-        high.innerHTML = commits[id].main.join('<br>');
-        mid.innerHTML = commits[id].main.join('<br>');
-        low.innerHTML = commits[id].main.join('<br>');
+        let inner = []; commits[id].main.map(i => inner += `<li>${i}</li>`); main.innerHTML = inner;
+        inner = []; commits[id].high.map(i => inner += `<li>${i}</li>`); high.innerHTML = inner;
+        inner = []; commits[id].mid.map(i => inner += `<li>${i}</li>`); mid.innerHTML = inner;
+        inner = []; commits[id].low.map(i => inner += `<li>${i}</li>`); low.innerHTML = inner;
     });
 
 const DELETE = async (id) => await fetch(url + id, { method: 'DELETE' });
@@ -27,5 +42,16 @@ const POST = async () => await fetch(url, {
     headers: {
         'Content-Type': 'application/json;charset=utf-8'
     },
-    body: JSON.stringify(test)
+    body: JSON.stringify()
 });
+
+const add = async (column) => {
+    let inner = `<li>
+        <div class='item'>
+            <div class='hash'>#</div>
+            <input class='tag' value='newtag' autofocus>
+            <div class='del' onclick="DELETE()">x</div>
+        </div>
+    </li>`
+    column.innerHTML += inner;
+}
