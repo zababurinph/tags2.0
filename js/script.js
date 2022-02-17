88a32c9777fe958d9607e8c473d093e7d1da4924
Tim Zababurin <raccoontd@mail.ru>
2022-02-17 14:17:04 +0300

    alfa-2.0.3

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-import { btnClick } from "./code.js";
-
 const url = 'https://620a9a0692946600171c5b7f.mockapi.io/db/tags/';
 const error404 = `<h1 style='margin: 5em 30px; text-align: center;'>
     В данный момент на сервере проводятся технические работы<br>
@@ -139,6 +137,52 @@ function allClicker(e, column, input, add, listener, alllistener) {
         console.log(data);
     }
     console.log('all');
+}
+
+//---------------------------------------------------------------
+const btnClick = (data) => {
+    let countHash = {
+        high: document.querySelector('#countHigh').value,
+        mid: document.querySelector('#countMid').value,
+        low: document.querySelector('#countLow').value
+    }
+
+    for (let key in countHash) {
+        if (countHash[key] === '' || countHash[key] < 0) {
+            alert(`Error: ошибка в заполнении`)
+            return;
+        };
+    }
+
+    for (let key in countHash) {
+        if (data[key].length < countHash[key]) {
+            alert(`Error: тэгов меньше, чем введенное число`)
+            return;
+        };
+    }
+
+    generate(data, countHash);
+}
+
+function shuffle(arr) {
+    let j;
+
+    for (var i = arr.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * i);
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+
+    return (arr);
+}
+
+function generate(data, countHash) {
+    let result = data.main;
+    result = result.concat(shuffle(data.high).slice(0, countHash.high));
+    result = result.concat(shuffle(data.mid).slice(0, countHash.mid));
+    result = result.concat(shuffle(data.low).slice(0, countHash.low));
+    
+    document.querySelector('#textarea').innerHTML = '';
+    result.forEach(e => document.querySelector('#textarea').innerHTML += "#" + e + " ");
 }
 
 //---------------------------------------------------------------
